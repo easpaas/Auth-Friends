83f761cf09a9a5e3c5679640ad9325f40074d9b8
evan <aspaasevan@gmail.com>
2020-04-21 15:36:13 -0700

    successful get with auth displays friends

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import '../App.css';
 
-const FriendsList = (props) => {
+import Friend from './Friend';
+
+const FriendsList = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+      
+    axios.get('http://localhost:5000/api/friends', {
+      headers: {
+        Authorization: token
+      }
+    })
+    .then(response => {
+      setData(response.data);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, []);
+
   return (
     <div className="List">
-      FriendsList
+    {
+      data.map(friend => {
+        return <Friend key={friend.id} name={friend.name} age={friend.age} email={friend.email} />
+      })
+    }
     </div>
   )
 }
